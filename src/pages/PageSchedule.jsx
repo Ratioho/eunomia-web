30a7c4e374d8a486ec7b218eb7c033a138aebebf
rethano <yuhsuanho@outlook.com>
2021-01-20 15:42:27 +0800

    feat: list item

--- src/pages/PageSchedule.jsx
+++ src/pages/PageSchedule.jsx
@@ -1,3 +1,55 @@
-const PageSchedule = () => <div>Schedule</div>
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const itemInfo = {
+  state: 'unfinished',
+  time: '1130',
+  content: 'Getup, shower, brush teeth',
+}
+
+const Wrapper = styled.div`
+  display: block;
+`
+
+const ScheduleItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: whitesmoke;
+  cursor: auto;
+`
+
+const ItemCheckbox = styled.div`
+  background: aqua;
+  cursor: pointer;
+`
+
+
+const ScheduleItem = ({info}) => {
+
+  const [completeStatus, setCompleteStatus] = useState(info.state)
+
+  return (
+    <ScheduleItemWrapper>
+      <ItemCheckbox onClick = {() => setCompleteStatus('finished')}>
+        {completeStatus === 'unfinished'
+          ? '口'
+          : 'X'}
+      </ItemCheckbox>
+      <div>{info.time}</div>
+      <div>{info.content}</div>
+    </ScheduleItemWrapper>
+  )
+}
+
+
+const PageSchedule = () => {
+  return (
+    <Wrapper>
+      <h2>Schedule Page: Now used for item test</h2>
+      <p>{JSON.stringify(itemInfo)}</p>
+      <ScheduleItem info = {itemInfo} />
+    </Wrapper>
+  )
+}
 
 export default PageSchedule
